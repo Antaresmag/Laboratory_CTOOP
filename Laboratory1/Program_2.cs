a82db9e56c7824dd156f61be093db1c46ead2747
Antaresmag <kunderenko2@gmail.com>
2020-06-03 19:02:57 +0300

    Додано 4 лабку

--- Laboratory1/Program_2.cs
+++ Laboratory1/Program_2.cs
@@ -6,5 +6,82 @@ namespace Laboratory1
 {
     class Program_2
     {
+        static void Main_2(string[] args)
+        {
+            mycar toyotaCar1 = new toyota();
+            toyotaCar1.ManufactureCar();
+            Console.WriteLine(toyotaCar1 + "\n");
+            Diesel carWithDieselEngine = new Diesel(toyotaCar1);
+            carWithDieselEngine.ManufactureCar();
+            Console.WriteLine();
+            mycar toyotaCar2 = new toyota();
+            Petrol carWithPetrolEngine = new Petrol(toyotaCar2);
+            carWithPetrolEngine.ManufactureCar();
+            Console.ReadKey();
+        }
+
+        class Diesel : CarDecorator
+        {
+            public Diesel(mycar car) : base(car)
+            {
+            }
+            public override mycar ManufactureCar()
+            {
+                car.ManufactureCar();
+                AddEngine(car);
+                return car;
+            }
+            public void AddEngine(mycar car)
+            {
+                if (car is toyota)
+                {
+                    toyota toyotaCar = (toyota)car;
+                    toyotaCar.Engine = "Дизельний двигун";
+                    Console.WriteLine("Diesel  додав двигун до авто : " + car);
+                }
+            }
+        }
+
+        class CarDecorator : mycar
+        {
+            protected mycar car;
+            public CarDecorator(mycar car)
+            {
+                this.car = car;
+            }
+            public virtual mycar ManufactureCar()
+            {
+                return car.ManufactureCar();
+            }
+        }
+
+        class toyota : mycar
+        {
+            private string CarName = "toyota";
+            public string CarBody { get; set; }
+            public string CarDoor { get; set; }
+            public string CarWheels { get; set; }
+            public string CarGlass { get; set; }
+            public string Engine { get; set; }
+            public override string ToString()
+            {
+                return "toyota [Марка автомобiля = " + CarName + ", Складається з = " + CarBody + ", Кiлькiсть дверей= " + CarDoor + ", Кiлькiсть колес = "
+                                + CarWheels + ", Кiлькiсть вiкон = " + CarGlass + ", Марка двигуна = " + Engine + "]";
+            }
+            public mycar ManufactureCar()
+            {
+                CarBody = "Метал";
+                CarDoor = "2 пари дверей";
+                CarWheels = "4 колеса ";
+                CarGlass = "6 вiкон";
+                return this;
+            }
+        }
+
+        public interface mycar
+        {
+            mycar ManufactureCar();
+        }
     }
 }
+
---
+++ Laboratory4/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Bridge
+{
+	public interface IReportFormat
+	{
+		string Content { get; set; }
+		void SaveReport(string reportName);
+	}
+
+	public class TxtReportFormat : IReportFormat
+	{
+		public string Content { get; set; }
+
+		public void SaveReport(string reportName)
+		{
+			File.WriteAllText(reportName + ".txt", Content);
+		}
+	}
+
+	public class HtmlReportFormat : IReportFormat
+	{
+		public string Content { get; set; }
+
+		public void SaveReport(string reportName)
+		{
+			File.WriteAllText(
+				reportName + ".html",
+				$"<h1>{ Content }</h1>".Replace("\n", "<br/>"));
+		}
+	}
+
+	public class Report
+	{
+		public IReportFormat ReportFormat;
+
+		public Report(IReportFormat reportFormat)
+		{
+			ReportFormat = reportFormat;
+		}
+
+		public virtual void CreateReport(string path)
+		{
+			ReportFormat.SaveReport(path);
+		}
+	}
+
+	public class DailyReport : Report
+	{
+		public Dictionary<string, int> DayResult = new Dictionary<string, int>();
+
+		public DailyReport(IReportFormat reportFormat) : base(reportFormat)
+		{
+		}
+
+		public override void CreateReport(string path)
+		{
+			ReportFormat.Content = "";
+
+			foreach (var pair in DayResult)
+			{
+				ReportFormat.Content += $"{ pair.Key }: { pair.Value }₴\n";
+			}
+
+			base.CreateReport(path);
+		}
+	}
+
+	public class WeeklyReport : Report
+	{
+		public int[] WeekResult = new int[7];
+
+		public WeeklyReport(IReportFormat reportFormat) : base(reportFormat)
+		{
+		}
+
+		public override void CreateReport(string path)
+		{
+			ReportFormat.Content = $"Понеділок: { WeekResult[0] }₴\n";
+			ReportFormat.Content += $"Вівторок: { WeekResult[1] }₴\n";
+			ReportFormat.Content += $"Середа: { WeekResult[2] }₴\n";
+			ReportFormat.Content += $"Четвер: { WeekResult[3] }₴\n";
+			ReportFormat.Content += $"П'ятниця: { WeekResult[4] }₴\n";
+			ReportFormat.Content += $"Субота: { WeekResult[5] }₴\n";
+			ReportFormat.Content += $"Неділя: { WeekResult[6] }₴";
+
+			base.CreateReport(path);
+		}
+	}
+
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			var dailyReport = new DailyReport(new TxtReportFormat())
+			{
+				DayResult = new Dictionary<string, int>
+				{
+					{ "Вологі серветки", 9800 },
+					{ "Носові серветки", 5600 },
+					{ "Рідке мило", 8350 },
+					{ "Дизинфікуючий засіб", 1100 }
+				}
+
+			};
+
+			dailyReport.CreateReport(DateTime.Now.ToString("dd-MM-yyyy" + "Дений звіт"));
+			dailyReport.ReportFormat = new HtmlReportFormat();
+			dailyReport.CreateReport(DateTime.Now.ToString("dd-MM-yyyy" + "Дений звіт"));
+
+			var weeklyReport = new WeeklyReport(new TxtReportFormat())
+			{
+				WeekResult = new int[7] { 21500, 18300, 20950, 17800, 22600, 19300, 19150 }
+			};
+
+			weeklyReport.CreateReport(DateTime.Now.ToString("dd-MM-yyyy" + "Тижневий звіт") + " " + DateTime.Now.ToString("dd-MM-yyyy" + "Тижневий звіт"));
+			weeklyReport.ReportFormat = new HtmlReportFormat();
+			weeklyReport.CreateReport(DateTime.Now.AddDays(-7).ToString("dd-MM-yyyy" + "Тижневий звіт") + " " + DateTime.Now.ToString("dd-MM-yyyy" + "Тижневий звіт"));
+
+			Console.WriteLine("Звіти створені! Можна закрити консоль і зайти у папаку Дебаг");
+
+			Console.ReadKey();
+		}
+	}
+}
